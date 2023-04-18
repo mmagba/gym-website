72c48dd87b6da54f908f4a658f21bb4e9cd14f0c
Mahmoud Agbaria <mahmoud.moaad@gmail.com>
2023-04-18 18:11:39 +0300

    Add 'gallery' page

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,7 +1,29 @@
 import './Gallery.css';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_3.jpg';
 
 const Gallery = () => {
-    return <h1>gallery page</h1>
+    const images = [];
+    const galleryLength = 15;
+
+    for (let i = 1; i <= galleryLength; i++) {
+        images.push(require(`../../images/gallery${i}.jpg`));
+    }
+
+    return <>
+        <Header image={HeaderImage} title='Our Gallery' content='Experience Fitness in Motion: Explore Our Gallery of Dynamic Workout Images and Inspire Your Fitness Journey Today!'></Header>
+        <section className='gallery'>
+            <div className='container gallery__container'>
+                {
+                    images.map((image, index) => {
+                        return <article key={index}>
+                            <img src={image} alt={`gallery image ${index + 1}`} />
+                        </article>
+                    })
+                }
+            </div>
+        </section>
+    </>
 };
 
 export default Gallery;

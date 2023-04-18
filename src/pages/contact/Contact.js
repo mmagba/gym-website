71266d9a2b9a81a6818db4129cbed36b14358b71
Mahmoud Agbaria <mahmoud.moaad@gmail.com>
2023-04-18 12:52:39 +0300

    Add 'contact' page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,7 +1,26 @@
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_2.jpg';
+import { MdEmail } from 'react-icons/md';
+import { BsMessenger } from 'react-icons/bs';
+import { IoLogoWhatsapp } from 'react-icons/io';
+
 import './Contact.css'
 
 const Contact = () => {
- return <h1>Contact Page</h1>
+    return <>
+        <Header title='Get In Touch' image={HeaderImage} content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
+        <section className='container contact__container'>
+            <a href='https://google.com' target='_blank' rel='noreferrer noopener'>
+                <MdEmail />
+            </a>
+            <a href='https://google.com' target='_blank' rel='noreferrer noopener'>
+                <BsMessenger />
+            </a>
+            <a href='https://google.com' target='_blank' rel='noreferrer noopener'>
+                <IoLogoWhatsapp />
+            </a>
+        </section>
+    </>
 };
 
 export default Contact;
